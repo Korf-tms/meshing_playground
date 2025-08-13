1e98364fb8b6fd969b0994f1d20b5edaa8091bbd
Tomáš Luber <tomas.luber@ugn.cas.cz>
2025-08-13 20:51:19 +0200

    Convex slopes and water

---
+++ src/convex_slope_with_waterlevels.py
@@ -0,0 +1,249 @@
+import gmsh
+import numpy as np
+
+
+def create_mesh(file_name='slope_with_waterlevels',
+                z_water_height=45, z_solid_water_level=80,
+                h=3):
+    def box_from_coordinates(coordinates):
+        """
+        Helper function to generate box from given coordinates of its vertices.
+        """
+        points = [factory.add_point(*c) for c in coordinates]
+        lines = [factory.add_line(points[i], points[(i+1)%4]) for i in range(4)]
+        lines += [factory.add_line(points[i+4], points[(i+1)%4+4]) for i in range(4)]
+        lines += [factory.add_line(points[i], points[i+4]) for i in range(4)]
+        surfaces = []
+        loops = [[0,1,2,3],[4,5,6,7],[0,8,4,9],[1,9,5,10],[2,10,6,11],[3,11,7,8]]
+        for loop_indices in loops:
+            cl = factory.add_curve_loop([lines[i] for i in loop_indices])
+            surfaces.append(factory.add_plane_surface([cl]))
+        sl = factory.add_surface_loop(surfaces)
+        return factory.add_volume([sl])
+
+    def find_outer_slope_face(volume_tag):
+        """
+        Finds the single "outer" sloped face of a volume by checking two criteria:
+        1. It must be an exterior face (adjacent to only one volume).
+        2. It must be slanted in all 3 dimensions (its bounding box is not flat).
+        """
+        boundaries = gmsh.model.getBoundary([(3, volume_tag)], oriented=False)
+        eps = 1e-6 # ok value for the slopes in meters
+
+        for dim, tag in boundaries:
+            adj_vols, _ = gmsh.model.getAdjacencies(dim, tag)
+            if len(adj_vols) == 1:  # It's an exterior face. Now check the geometry.
+                min_x, min_y, min_z, max_x, max_y, max_z = gmsh.model.getBoundingBox(dim, tag)
+
+                # Criterion 2: Is it slanted in all 3 dimensions?
+                is_sloped_in_x = (max_x - min_x) > eps
+                is_sloped_in_y = (max_y - min_y) > eps
+                is_sloped_in_z = (max_z - min_z) > eps
+
+                if is_sloped_in_x and is_sloped_in_y and is_sloped_in_z:  # there is only one suitable face
+                    return tag
+        return -1 # Return -1 if not found, should actually raise some error here
+
+    gmsh.initialize()
+    factory = gmsh.model.occ
+
+    # parameters of 3d geometry
+    x_slope = 60  # 15, 35, 60
+    x_top = 85
+    x_down = 30
+    x_cover = 5
+    z_layer0 = 20
+    z_layer1 = 10
+    z_layer2 = 30
+    slope_half_width = 50
+    turning_corner = 120  # 90, 120, 135, 150, 270, 240, 225, 210
+
+    z_sum = z_layer0 + z_layer1 + z_layer2
+    concave = False
+    angle = (360-turning_corner)/2/180*np.pi
+    if turning_corner < 180:
+        concave = True
+        angle = turning_corner/2/180*np.pi
+    y_half = slope_half_width * np.sin(angle)
+    y_sum = 2*y_half
+    x_cutout = slope_half_width * np.cos(angle)
+    x_sum = x_top + x_slope + x_cutout + x_cover + x_down  # discrepancy in the paper, x_cutout added
+    print(x_sum, y_sum, z_sum)
+
+
+    bottom_layer = factory.add_box(0, 0, 0, x_sum, y_sum, z_layer0)
+    middle_layer = factory.add_box(0, 0, z_layer0, x_sum, y_sum, z_layer1)
+    top_layer = factory.add_box(0, 0, z_layer0+z_layer1, x_sum, y_sum, z_layer2)
+
+    # prepare deformed box to cut out from the top layer (left side):
+    coordinates_cutout_left = [[x_sum - x_down - x_cover, 0, z_layer0+z_layer1], [x_sum, 0, z_layer0+z_layer1],
+                            [x_sum, y_half, z_layer0+z_layer1], [x_sum - x_down - x_cover - x_cutout, y_half, z_layer0+z_layer1],
+                            [x_top + x_cutout, 0, z_sum], [x_sum, 0, z_sum],
+                            [x_sum, y_half, z_sum], [x_top, y_half, z_sum]]
+    cutout_left = box_from_coordinates(coordinates_cutout_left)
+
+    # right side
+    cutout_right_with_tag = factory.copy([(3, cutout_left)])
+    factory.mirror(cutout_right_with_tag, 0, 1, 0, -y_half)
+
+    # left side of slope cover:
+    coordinates_cover_left = [[x_sum - x_down - x_cover, 0, z_layer0+z_layer1], [x_sum - x_down, 0, z_layer0+z_layer1],
+                            [x_sum - x_down - x_cutout, y_half, z_layer0+z_layer1], [x_sum - x_down - x_cover - x_cutout, y_half, z_layer0+z_layer1],
+                            [x_top + x_cutout, 0, z_sum], [x_top + x_cutout + x_cover, 0, z_sum],
+                            [x_top + x_cover, y_half, z_sum], [x_top, y_half, z_sum]]
+    cover_left = box_from_coordinates(coordinates_cover_left)
+
+    # right side:
+    cover_right_with_tag = factory.copy([(3, cover_left)])
+    cover_right = cover_right_with_tag[0][1]
+    factory.mirror(cover_right_with_tag, 0, 1, 0, -y_half)
+
+    if concave:  # swap left and right side
+        factory.translate([(3, cutout_left)], 0, y_half, 0)
+        factory.translate(cutout_right_with_tag, 0, -y_half, 0)
+        factory.translate([(3, cover_left)], 0, y_half, 0)
+        factory.translate(cover_right_with_tag, 0, -y_half, 0)
+
+    # cut out prepared deformed boxes:
+    cut_output = factory.cut([(3, top_layer)], [(3, cutout_left)]+cutout_right_with_tag)
+    top_layer_final = cut_output[0][0][1]
+
+    # Puts the volumes together
+    final_volumes_to_heal = [(3, bottom_layer), (3, middle_layer), (3, top_layer_final), (3, cover_left), (3, cover_right)]
+    out_tags, out_map = factory.fragment(final_volumes_to_heal, [])
+    factory.synchronize()
+    # here, out map is the same as the input, everything retains its tag, so no action needed
+    print(final_volumes_to_heal)
+    print(out_map)
+
+    # no renumbering needed here as well
+    new_cover_left_tag = cover_left
+    new_cover_right_tag = cover_right
+
+    # Find the specific faces using the helper function
+    slope_face_left_tag = find_outer_slope_face(new_cover_left_tag)
+    slope_face_right_tag = find_outer_slope_face(new_cover_right_tag)
+
+    if slope_face_left_tag == -1 or slope_face_right_tag == -1:  # should raise some errors here
+        print('ERROR: Failed to find the outer slope surfaces on the healed model.')
+
+    # --- Intersect the faces with a plane ---
+    eps = 10
+    p1 = factory.add_point(0-eps, 0-eps, z_water_height); p2 = factory.add_point(x_sum+eps, 0-eps, z_water_height)
+    p3 = factory.add_point(x_sum+eps, y_sum+eps, z_water_height); p4 = factory.add_point(0-eps, y_sum+eps, z_water_height)
+    cutting_plane_loop = factory.add_curve_loop([factory.add_line(p1,p2), factory.add_line(p2,p3), factory.add_line(p3,p4), factory.add_line(p4,p1)])
+    cutting_plane_surface = factory.add_plane_surface([cutting_plane_loop])
+
+    # Use fragment to split the faces
+    out_tags_split, _ = factory.fragment(
+        [(2, slope_face_left_tag)], # Objects to split
+        [(2, cutting_plane_surface)],  # Tool
+    )
+    # The output will contain the new upper and lower face parts, and the new intersection object.
+    upper_l, lower_l = -1, -1
+    for item in out_tags_split:
+        center_coo = factory.getCenterOfMass(*item)
+        if np.isclose(center_coo[2], z_water_height):  # intersection in the plane, we do not want that
+            pass
+        elif center_coo[2] > z_water_height:
+            upper_l = item[1]
+        else:
+            lower_l = item[1]
+    print(upper_l, lower_l)
+
+    # the same thing for the other side
+    out_tags_split, _ = factory.fragment(
+        [(2, slope_face_right_tag)], # Objects to split
+        [(2, cutting_plane_surface)],  # Tool
+    )
+
+    upper_r, lower_r = -1, -1
+    for item in out_tags_split:
+        center_coo = factory.getCenterOfMass(*item)
+        if np.isclose(center_coo[2], z_water_height):
+            pass
+        elif center_coo[2] > z_water_height:
+            upper_r = item[1]
+        else:
+            lower_r = item[1]
+    print(upper_r, lower_r)
+
+    # remove the auxiliary plane
+    factory.remove([(2, cutting_plane_surface)], recursive=True)
+    factory.synchronize()
+
+    # deal with the cut at x = 0
+    all_faces = gmsh.model.getEntities(dim=2)
+    max_z_center = -1
+    x_faces = []
+    # we must pick the correct face to cut depending on z_solid_water_level!
+    # currently provided by naked eye that the cut is in the topmost face
+    for dim, tag in all_faces:
+        center_coo = factory.get_center_of_mass(dim, tag)
+        if abs(center_coo[0]) < 1e-6:  # face is at the x=0 plane
+            x_faces.append(tag)
+            if max_z_center < center_coo[2]:
+                max_z_center = center_coo[2]
+                top_face = tag
+    x_faces.remove(top_face)
+
+    # create another auxiliray plane, shameless copy&paste
+    z_water_height = z_solid_water_level
+    eps = 10
+    p1 = factory.add_point(0-eps, 0-eps, z_water_height); p2 = factory.add_point(x_sum+eps, 0-eps, z_water_height)
+    p3 = factory.add_point(x_sum+eps, y_sum+eps, z_water_height); p4 = factory.add_point(0-eps, y_sum+eps, z_water_height)
+    cutting_plane_loop = factory.add_curve_loop([factory.add_line(p1,p2), factory.add_line(p2,p3), factory.add_line(p3,p4), factory.add_line(p4,p1)])
+    cutting_plane_surface = factory.add_plane_surface([cutting_plane_loop])
+
+    out_tags_split, _ = factory.fragment(
+        [(2, top_face)], # Objects to split
+        [(2, cutting_plane_surface)],  # Tool
+    )
+
+    upper_x0, lower_x0 = -1, -1
+    for item in out_tags_split:
+        center_coo = factory.getCenterOfMass(*item)
+        if np.isclose(center_coo[2], z_water_height):
+            pass
+        elif center_coo[2] > z_water_height:
+            upper_x0 = item[1]
+        else:
+            lower_x0 = item[1]
+    print(upper_x0, lower_x0)
+
+    # remove the auxiliary plane
+    factory.remove([(2, cutting_plane_surface)], recursive=True)
+    factory.synchronize()
+
+    x_faces.append(lower_x0)
+
+    # Assign physical groups
+    gmsh.model.addPhysicalGroup(3, [bottom_layer], name="bottom_layer")
+    gmsh.model.addPhysicalGroup(3, [middle_layer], name="middle_layer")
+    gmsh.model.addPhysicalGroup(3, [top_layer_final], name="top_layer")
+    gmsh.model.addPhysicalGroup(3, [new_cover_left_tag, new_cover_right_tag], name="cover")
+    gmsh.model.addPhysicalGroup(2, [upper_l, upper_r], name='dry_slope')
+    gmsh.model.addPhysicalGroup(2, [lower_l, lower_r], name='wet_slope')
+    gmsh.model.addPhysicalGroup(2, [upper_x0], name='dry_solid')
+    gmsh.model.addPhysicalGroup(2, x_faces, name='wet_solid')
+
+    factory.synchronize()
+
+    # set sizes and generate mesh
+    gmsh.model.mesh.setSize(gmsh.model.getEntities(0), h)
+    gmsh.model.mesh.generate(3)
+    gmsh.write(f"{file_name}.msh")
+    print(f"Mesh written to {file_name}")
+    gmsh.fltk.run()
+    gmsh.finalize()
+
+
+def transform_to_hdf5(input_file='slope_final.msh'):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    create_mesh()
+    transform_to_hdf5()
